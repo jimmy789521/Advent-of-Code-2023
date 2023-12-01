34cc06e47e90b19bffbf7ff81bcb228e8bd7bb84
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2023-12-01 16:35:14 +0100

    Added day one solution for both puzzles

---
+++ Day1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static int GetLineNumber(string line)
+        {
+            line = Regex.Replace(line, "one", "o1e");
+            line = Regex.Replace(line, "two", "t2o");
+            line = Regex.Replace(line, "three", "t3e");
+            line = Regex.Replace(line, "four", "f4r");
+            line = Regex.Replace(line, "five", "f5e");
+            line = Regex.Replace(line, "six", "s6x");
+            line = Regex.Replace(line, "seven", "s7n");
+            line = Regex.Replace(line, "eight", "e8t");
+            line = Regex.Replace(line, "nine", "n9e");
+            line = Regex.Replace(line, "[^0-9]", "");
+            string number = line[0].ToString() + line[line.Length - 1].ToString();
+            return Int32.Parse(number);
+        }
+    }
+}
---
+++ Day1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Advent_of_Code_2023;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Debt
+        {
+            get { return (string)GetValue(DebtProperty); }
+            set { SetValue(DebtProperty, value); }
+        }
+        public static readonly DependencyProperty DebtProperty =
+                    DependencyProperty.Register("Debt", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+        public MainWindow()
+        {
+            InitializeComponent();
+            string fileName = "C:\\Users\\Jimmy BERNABE\\source\\repos\\Advent of Code 2023\\Advent of Code 2023\\Input.txt";
+            int sum = 0;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    sum = sum + App.GetLineNumber(line);
+                }
+            }
+
+            this.DataContext = this;
+            this.Debt = sum.ToString();
+        }
+    }
+}

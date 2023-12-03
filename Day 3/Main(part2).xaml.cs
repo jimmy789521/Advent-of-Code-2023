eebf359a9d3d6248f466099943509b06363d085e
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2023-12-03 16:25:33 +0100

    Add solution for puzzle 2 of day 3

---
+++ Day 3/Main(part2).xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Advent_of_Code_2023;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Debt
+        {
+            get { return (string)GetValue(DebtProperty); }
+            set { SetValue(DebtProperty, value); }
+        }
+        public static readonly DependencyProperty DebtProperty =
+                    DependencyProperty.Register("Debt", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+        public MainWindow()
+        {
+            InitializeComponent();
+            string fileName = "C:\\Users\\Jimmy BERNABE\\source\\repos\\Advent of Code 2023\\Advent of Code 2023\\Input.txt";
+            IDictionary<string, List<int>> gears = new Dictionary<string, List<int>>();
+            int lineNum = -1;
+            string prevline = "";
+            string curline = "";
+            string nextline = "";
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lineNum++;
+                    prevline = curline;
+                    curline = nextline;
+                    nextline = line;
+                    if (String.IsNullOrWhiteSpace(curline))
+                        continue;
+                    gears = App.GetGears(prevline, curline, nextline, gears, lineNum);
+                }
+                gears = App.GetGears(curline, nextline, "", gears, lineNum+1);
+            }
+            int sum = 0;
+            foreach (var list in gears.Values)
+            {
+               if (list.Count == 2)
+                {
+                    sum += list[0] * list[1];
+                } 
+            }
+            this.DataContext = this;
+            this.Debt = sum.ToString();
+        }
+    }
+}
---
+++ Day 3/Program(part2).xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IDictionary<string, List<int>> GetGears(string prevline, string curline, string nextline, IDictionary<string, List<int>> gears,int lineNum)
+        {
+            string numMem = "";
+            string pos = "";
+            for (int i = 0; i < curline.Length; i++)
+            {
+                if (Char.IsNumber(curline[i])) //begin or middle of a number
+                {
+                    numMem += curline[i]; // this operation allows to remember the full number until it's either added to the sum, or dropped if not valid
+                    for (int j = Math.Max(0, i - 1); j < Math.Min(i + 2, curline.Length); j++) //Check the surroundings if there is a gear
+                    {
+                        if (!String.IsNullOrWhiteSpace(prevline))
+                        {
+                            if (prevline[j] == '*')
+                            {
+                                pos = (lineNum-1).ToString() + '-' + j.ToString(); //Position of the gear linked to this number
+                            }
+                        }
+                        if (!String.IsNullOrWhiteSpace(nextline))
+                        {
+                            if (nextline[j] == '*')
+                            {
+                                pos = (lineNum + 1).ToString() + '-' + j.ToString();
+                            }
+                        }
+                        if (curline[j] == '*')
+                        {
+                            pos = lineNum.ToString() + '-' + j.ToString();
+                        }
+                    }
+                }
+                else // No number or end of the number
+                {
+                    if (!String.IsNullOrWhiteSpace(pos)) // there was a number linked to a gear
+                    {
+                        if (gears.ContainsKey(pos)) // the gears is known, the number is added to the list of numbers linked to the gear
+                        {
+                            List<int> list = gears[pos];
+                            list.Add(Int32.Parse(numMem));
+                        }
+                        else
+                        {
+                            List<int> list = new List<int>(); // new gear
+                            list.Add(Int32.Parse(numMem));
+                            gears.Add(pos, list);
+                        }
+                    }
+                    numMem = ""; //Reset to default value until next number in the line
+                    pos = "";
+                }
+            }
+            if (!String.IsNullOrWhiteSpace(pos)) //The last value was a number linked to a gear
+            {
+                if (!String.IsNullOrWhiteSpace(pos))
+                {
+                    if (gears.ContainsKey(pos))
+                    {
+                        List<int> list = gears[pos];
+                        list.Add(Int32.Parse(numMem));
+                    }
+                    else
+                    {
+                        List<int> list = new List<int>();
+                        list.Add(Int32.Parse(numMem));
+                        gears.Add(pos, list);
+                    }
+                }
+                numMem = ""; //Reset to default value until next number in the line
+                pos = "";
+            }
+            return gears;
+        }
+    }
+}

fd22d29e073178c1fb0db63ce01150890421262a
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2023-12-03 10:11:06 +0100

    Add the solution to the first puzzle of day 3

---
+++ Day 3/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Advent_of_Code_2023;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Debt
+        {
+            get { return (string)GetValue(DebtProperty); }
+            set { SetValue(DebtProperty, value); }
+        }
+        public static readonly DependencyProperty DebtProperty =
+                    DependencyProperty.Register("Debt", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+        public MainWindow()
+        {
+            InitializeComponent();
+            string fileName = "C:\\Users\\Jimmy BERNABE\\source\\repos\\Advent of Code 2023\\Advent of Code 2023\\Input.txt";
+            int sum = 0;
+            string prevline = "";
+            string curline = "";
+            string nextline = "";
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    prevline = curline;
+                    curline = nextline;
+                    nextline = line;
+                    if (String.IsNullOrWhiteSpace(curline))
+                        continue;
+                    sum = sum + App.GetParts(prevline, curline, nextline);
+                }
+                sum = sum + App.GetParts(curline, nextline, "");
+            }
+
+            this.DataContext = this;
+            this.Debt = sum.ToString();
+        }
+    }
+}
---
+++ Day 3/Program(part1).xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static int GetParts(string prevline, string curline, string nextline)
+        {
+            int ans = 0;
+            string numMem = "";
+            bool valid = false;
+            for (int i = 0; i < curline.Length; i++)
+            {
+                if (Char.IsNumber(curline[i])) //begin or middle of a number
+                {
+                    numMem+= curline[i]; // this operation allows to remember the full number until it's either added to the sum, or dropped if not valid
+                    for(int j = Math.Max(0,i-1); j<Math.Min(i+2,curline.Length); j++) //Check the surroundings if there is a symboles
+                    {
+                        if (!String.IsNullOrWhiteSpace(prevline))
+                        {
+                            if (!Char.IsNumber(prevline[j]) && prevline[j] != '.')
+                            {
+                                valid= true;
+                            }
+                        }
+                        if (!String.IsNullOrWhiteSpace(nextline))
+                        {
+                            if (!Char.IsNumber(nextline[j]) && nextline[j] != '.')
+                            {
+                                valid = true;
+                            }
+                        }
+                        if (!Char.IsNumber(curline[j]) && curline[j] != '.')
+                        {
+                            valid = true;
+                        }
+                    }
+                }
+                else // No number or end of the number
+                {
+                    if (valid)
+                    {
+                        ans += Int32.Parse(numMem); // the number is valid, it is added to the value of the line.
+                    }
+                    numMem = ""; //Reset to default value until next number in the line
+                    valid = false;
+                }
+            }
+            if (valid)
+            {
+                ans += Int32.Parse(numMem); // In case the line finishes on a valid number
+            }
+            return ans;
+        }
+    }
+}

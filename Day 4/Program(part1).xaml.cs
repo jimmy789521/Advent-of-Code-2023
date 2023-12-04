6c62cdba618dcb01fa36f78930a77a55b48e495e
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2023-12-04 10:32:09 +0100

    add solution for the first puzzle of day 4

---
+++ Day 4/Program(part1).xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static int CheckCard(string line)
+        {
+            line = line.Replace("  ", " ");
+            line = line.Substring(9); // We remove the card name and all double spaces
+            string[] winNums = line.Split(" | ")[0].Split(' ');
+            string[] drawNums = line.Split(" | ")[1].Split(' ');
+            string[] commonNum = winNums.Intersect(drawNums).ToArray(); // We get an array of all the numbers in common
+            if(commonNum.Length > 0 )
+            {
+                return (int)Math.Pow(2,commonNum.Length-1); //We calculate the points based on the amount of matching numbers.
+            }
+            else { return 0; }
+        }
+    }
+}

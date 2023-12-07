c275a8dc021e96963396975628fa158da96c31ad
jimmy789521 <71819826+jimmy789521@users.noreply.github.com>
2023-12-07 09:34:42 +0100

    Add solution to puzzle one and two of day 6

---
+++ Day 6/Program(part1&2).xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Advent_of_Code_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static int WinRace(long time, long distance)
+        {
+            int ans = 0;
+            for (long i = 0; i<time+1; i++)
+            {
+                if(i * (time - i)> distance)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
